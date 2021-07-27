ee1f6a4b78f20beb44c11282e26432871140729c
BeSketens <87710895+BeSketens@users.noreply.github.com>
2021-07-27 16:26:56 +0200

    Update fuel_server.lua

--- source/fuel_server.lua
+++ source/fuel_server.lua
@@ -1,14 +1,42 @@
 RegisterServerEvent('fuel:getPlayerMoney')
 AddEventHandler('fuel:getPlayerMoney', function(source)
-    exports.SQLquerries:getMoney(source, function(money)
-        TriggerClientEvent('fuel:sendPlayerMoney', source, money)
-    end)
+	function getMoney(source, callback)
+	    local steam = exports.SQLquerries:steamID(source)
+	    MySQL.Async.fetchAll("SELECT money FROM users WHERE identifier = @identifier", 
+				{['@identifier'] = steam },
+			    function(result)
+				if result[1] then 
+				    callback(result[1].money)
+				else 
+				    callback(nil)
+				end
+			    end
+	    )
+	end
+   	getMoney(source, function(money)
+        	TriggerClientEvent('fuel:sendPlayerMoney', source, money)
+    	end)
 end)
 
 RegisterServerEvent('fuel:setPlayerMoney')
 AddEventHandler('fuel:setPlayerMoney', function(source, money)
 	local amount = Round(money)
-	exports.SQLquerries:setMoney(source, false, amount)
+	function setMoney(source, bool, amount)
+		local steam = exports.SQLquerries:steamID(source)
+		local diff
+		if bool then
+			diff = amount
+		else
+			diff = 0 - amount
+		end
+		MySQL.Async.execute('UPDATE users SET money = (money + (@amount)) WHERE identifier = @identifier',
+				{
+					['@amount'] = diff, 
+					['@identifier'] = steam
+				},
+				function(result) end)
+	end
+	setMoney(source, false, amount)
 end)
 
 -----------
